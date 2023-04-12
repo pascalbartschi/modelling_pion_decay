d973a37465f62b467df5ccb4aaa80ca65bf64e34
Levin Linus Martschke <levinlinus.martschke@uzh.ch>
2023-04-12 14:33:23 +0200

    Replace DA_projectIV.py

--- DA_projectIV.py
+++ DA_projectIV.py
@@ -15,15 +15,7 @@ def N(t): # exp. decay function (eq. (1) )
 def one_over_N(t): # need this to find the max. of N(t) for the acc-reject method as N(t=0) != max(N(t))
     return 1/N(t)
 
-np.random.seed(42) # setting random seed for reprocucibility
-
-# plotting the function N(t) to understand what it looks like, will delete later
-x = np.linspace(0,1e-5,100000)
-plt.plot(x,N(x),".",markersize=1)
-plt.xlabel("t")
-plt.ylabel("Counts")
-plt.show()
-plt.clf()
+np.random.seed(42) # setting random seed for reproducibility
 
 # generate 10k decay times according to N(t) using accept-reject method
 accepted_t_vals = []
@@ -31,17 +23,46 @@ t_0 = 1e-30
 t_bound = [(1e-30,3e-5)]
 n_max = 1/(optimize.minimize(one_over_N,x0=t_0,bounds=t_bound)["fun"]) # minimising 1/N = maximising N
 i=0
+t_start = 0
+t_end = 3e-5
 while i < 10000:
-    t_i = np.random.uniform(0,3e-5)
+    t_i = np.random.uniform(t_start,t_end)
     y_i = np.random.uniform(0,n_max)
     if N(t_i) > y_i:
         accepted_t_vals.append(t_i)
         i += 1
 
-plt.hist(accepted_t_vals,bins=30,label="Generated decay times",alpha=0.5)
+# plotting the histogram of the decay times
+num_of_bins = 30
+counts, edges = np.histogram(accepted_t_vals,bins=num_of_bins,range=(t_start,t_end))
+binwidth = (t_end-t_start)/num_of_bins
+bin_centres = edges[:-1]+binwidth/2
+plt.bar(bin_centres,counts,width=binwidth,label="Generated decay times",alpha=0.5)
+#plt.hist(accepted_t_vals,bins=num_of_bins,label="Generated decay times",alpha=0.5)
 plt.xlabel("t [s]")
 plt.ylabel("Number of entries")
 plt.title("3(a) Histogram of 10'000 simulated decay times")
 plt.legend()
 plt.show()
 plt.clf()
+
+#--------
+# 3(b)
+def N(t,tau_mu,tau_pi):
+    return (N_0/(tau_mu-tau_pi) * (np.exp(-t/tau_mu)-np.exp(-t/tau_pi)))
+
+data = np.array(accepted_t_vals)
+
+def nll(params):
+    tau_mu,tau_pi = params
+    pdf = N(bin_centres,tau_mu,tau_pi)
+    prediction = len(data)*pdf*((t_end-t_start)/num_of_bins) # = f_i | last factor = delta x = binwidth
+    summands = counts*np.log(prediction)-prediction
+    return -summands.sum()
+
+# minimising binned nll, bounds and starting values arbitrarily chosen...
+bounds = [(tau_muon-10*tau_muon_uncert,tau_muon+10*tau_muon_uncert),(tau_pion-10*tau_pion_uncert,tau_pion+10*tau_pion_uncert)]
+tau_0 = [(tau_muon-10*tau_muon_uncert,tau_pion-10*tau_pion_uncert)]
+function_to_be_minimised = nll # otherwise will not get anything because likelihood way too small
+result = optimize.minimize(function_to_be_minimised,x0=tau_0,method="SLSQP",bounds=bounds)
+print("Best values for tau_mu, tau_pi:",result["x"])
