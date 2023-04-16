20ab0ea7facb121f5bc56af738185c2a532e0f37
Antonio Del Rio <antonio.delrio@business.uzh.ch>
2023-04-16 17:15:51 +0200

    Upload New File

---
+++ DA_projectIV_V2.py
@@ -0,0 +1,102 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy import optimize as opt
+
+tMuon = 2.1969811e-6 # [s]
+tMuonUncert = 0.0000022e-6 # [s]
+
+tPion = 8.43e-17 # [s]
+tPionUncert = 0.13e-17 # [s]
+
+def N(t, t_muon, t_pion, N0=1): # exp. decay function (eq. (1) )
+    return (N0/(t_muon-t_pion) * (np.exp(-t/t_muon)-np.exp(-t/t_pion)))
+
+def inverseN(t, t_muon, t_pion): # need this to find the max. of N(t) for the acc-reject method as N(t=0) != max(N(t))
+    return 1/N(t, t_muon, t_pion)
+
+def randVals(t_muon, t_pion):
+    tVals = []
+    t0 = 1e-10
+    tBounds = [(1e-10,3e-5)]
+    minVal = opt.minimize(inverseN, x0=t0, args=(t_muon, t_pion), bounds=tBounds)["fun"]
+    nMax = 1/minVal
+    
+    i = 0
+    tStart = 0
+    tEnd = 3e-5  
+    
+    while i < 10000:
+        t_i = np.random.uniform(tStart,tEnd)
+        y_i = np.random.uniform(0,nMax)
+        if N(t_i, t_muon, t_pion) > y_i:
+            tVals.append(t_i)
+            i += 1    
+            
+    return(tVals, tStart, tEnd)
+
+def plotHist(numBins=30):
+    tVals, tStart, tEnd = randVals(tMuon, tPion)
+    counts, edges = np.histogram(tVals, bins=numBins, range=(tStart, tEnd))
+    binWidth = (tEnd-tStart)/numBins
+    binCentre = edges[:-1] + binWidth/2
+    plt.bar(binCentre, counts, width=binWidth, label="Generated Decay Times", alpha=0.5)
+    plt.xlabel("t [s]")
+    plt.ylabel("Number of entries")
+    plt.title("3(a) Histogram of 10'000 simulated decay times")
+    plt.legend()
+    plt.savefig("Exercise 3a.png")
+    plt.clf()
+    #plt.show()
+    
+
+
+
+def binnedNNL(parms, binCentre, tStart, tEnd, numBins, lenData, counts):
+    t_muon, t_pion = parms
+    pdf = N(binCentre, t_muon, t_pion)
+    prediction = lenData*pdf*((tEnd-tStart)/numBins) # = f_i | last factor = delta x = binwidth
+    summands = counts*np.log(prediction)-prediction
+    return -summands.sum()
+
+def tauEstimates(t_muon, t_muon_uncert, t_pion, t_pion_uncert):
+    tVals, tStart, tEnd = randVals(t_muon, t_pion)
+    lenData = len(tVals)
+    numBins = 30
+    counts, edges = np.histogram(tVals, bins=numBins, range=(tStart, tEnd))
+    binWidth = (tEnd-tStart)/numBins
+    binCentre = edges[:-1] + binWidth/2
+    
+    bounds = [(t_muon-10*t_muon_uncert,t_muon+10*t_muon_uncert),(t_pion-10*t_pion_uncert,t_pion+10*t_pion_uncert)]
+    tau0 = [(t_muon-10*t_muon_uncert,t_pion-10*t_pion_uncert)]
+    result = opt.minimize(binnedNNL,x0=tau0, args=(binCentre, tStart, tEnd, numBins, lenData, counts), method="SLSQP",bounds=bounds)
+    tau_muon_est = result["x"][0]
+    tau_pion_est = result["x"][1]
+    return tau_muon_est, tau_pion_est
+
+
+print("Exercise 3a)")
+np.random.seed(42)
+plotHist()
+print("Graph generated and saved.")
+print("")
+
+tMuon3b, tPion3b = tauEstimates(tMuon, tMuonUncert, tPion, tPionUncert)
+print("Exercise 3b)")
+print(f"Estimated lifetime of muon: {tMuon3b}s")
+print(f"Estimated lifetime of pion: {tPion3b}s")
+print("")
+
+
+print("Exercise 3c)")
+#np.random.seed()
+
+# many measurements..?
+tMuonVals = []
+tPionVals = []
+for i in range(10):
+    tMuonEst, tPionEst = tauEstimates(tMuon, tMuonUncert, tPion, tPionUncert)
+    tMuonVals.append(tMuonEst)
+    tPionVals.append(tPionEst)
+    print(i)
+
+print(tMuonVals)
