cad827f7d1544d81725abdada508dc5b08ceed17
unknown <levinlinus.martschke@uzh.ch>
2023-04-17 14:24:02 +0200

    defined some varibles globally because otherwise I need to call 3 functions just to get a thing that was already calculated before. Also added smth to find the uncertainty on the tau estimates but it's not finished yet.

--- DA_projectIV_V2.py
+++ DA_projectIV_V2.py
@@ -14,6 +14,9 @@ def N(t, t_muon, t_pion, N0=1): # exp. decay function (eq. (1) )
 def inverseN(t, t_muon, t_pion): # need this to find the max. of N(t) for the acc-reject method as N(t=0) != max(N(t))
     return 1/N(t, t_muon, t_pion)
 
+tStart = 0 # need this as global variable.
+tEnd = 3e-5  
+
 def randVals(t_muon, t_pion):
     tVals = []
     t0 = 1e-10
@@ -22,20 +25,18 @@ def randVals(t_muon, t_pion):
     nMax = 1/minVal # max of decay function, needed for accept-reject method
     
     i = 0
-    tStart = 0
-    tEnd = 3e-5  
-    
     while i < 10000: # accept-reject method to get 10k t values according to the decay function
         t_i = np.random.uniform(tStart,tEnd)
         y_i = np.random.uniform(0,nMax)
         if N(t_i, t_muon, t_pion) > y_i:
             tVals.append(t_i)
             i += 1    
-            
-    return(tVals, tStart, tEnd)
+    return tVals
+
+tVals_3a = randVals(tMuon,tPion)
 
 def plotHist(numBins=30): # plotting the accepted decay times (= t values) in a histogram to then perform the binned neg. log. likelih.
-    tVals, tStart, tEnd = randVals(tMuon, tPion)
+    tVals = randVals(tMuon, tPion)
     counts, edges = np.histogram(tVals, bins=numBins, range=(tStart, tEnd))
     binWidth = (tEnd-tStart)/numBins
     binCentre = edges[:-1] + binWidth/2
@@ -49,9 +50,7 @@ def plotHist(numBins=30): # plotting the accepted decay times (= t values) in a 
     #plt.show()
     
 
-
-
-def binnedNNL(parms, binCentre, tStart, tEnd, numBins, lenData, counts):
+def binnedNLL(parms, binCentre, tStart, tEnd, numBins, lenData, counts):
     """binned negative log likelihood to find estimates of tau_mu & tau_pi from the 10'000 t values we created before and plotted in a histogram. We get the estimates by minimising the nll."""
     t_muon, t_pion = parms
     pdf = N(binCentre, t_muon, t_pion)
@@ -60,7 +59,7 @@ def binnedNNL(parms, binCentre, tStart, tEnd, numBins, lenData, counts):
     return -summands.sum()
 
 def tauEstimates(t_muon, t_muon_uncert, t_pion, t_pion_uncert):
-    tVals, tStart, tEnd = randVals(t_muon, t_pion)
+    tVals = randVals(t_muon, t_pion)
     lenData = len(tVals)
     numBins = 30
     counts, edges = np.histogram(tVals, bins=numBins, range=(tStart, tEnd))
@@ -69,7 +68,7 @@ def tauEstimates(t_muon, t_muon_uncert, t_pion, t_pion_uncert):
     
     bounds = [(t_muon-10*t_muon_uncert,t_muon+10*t_muon_uncert),(t_pion-10*t_pion_uncert,t_pion+10*t_pion_uncert)]
     tau0 = [(t_muon-10*t_muon_uncert,t_pion-10*t_pion_uncert)]
-    result = opt.minimize(binnedNNL,x0=tau0, args=(binCentre, tStart, tEnd, numBins, lenData, counts), method="SLSQP",bounds=bounds)
+    result = opt.minimize(binnedNLL,x0=tau0, args=(binCentre, tStart, tEnd, numBins, lenData, counts), method="SLSQP",bounds=bounds)
     tau_muon_est = result["x"][0]
     tau_pion_est = result["x"][1]
     return tau_muon_est, tau_pion_est
