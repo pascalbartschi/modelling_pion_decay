224daf360d08870d87206c76b01dcb41af7a91a7
Lukasz Melchisedekow <lukasz.melchisedekow@uzh.ch>
2023-05-02 10:42:53 +0200

    accept reject uniform

--- lukasz_da.py
+++ lukasz_da.py
@@ -12,3 +12,14 @@ tBound = (1e-10, 3e-5)
 # decay function
 def N(t, param, N0=1) :
     return (N0/(param[0]-param[1]))*(np.exp(-t/param[0]) - np.exp(-t/param[1]))
+
+# accept reject uniform
+def accept_uni(pdf, param, lowhi) :
+    lx, hx, ly, hy = lowhi
+    while True :
+        x = np.random.uniform(low=lx, high=hx)
+        y = np.random.uniform(low=ly, high=hy)
+        if pdf(x, param) > y :
+            break
+    return x
+        
