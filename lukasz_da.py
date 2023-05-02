c58db7f8402bbf660c9883ca5bf7bae4c1b6fd76
Lukasz Melchisedekow <lukasz.melchisedekow@uzh.ch>
2023-05-02 10:42:02 +0200

    create global variables

--- lukasz_da.py
+++ lukasz_da.py
@@ -1,3 +1,11 @@
 import numpy as np
 from matplotlib import pyplot as plt
 from scipy import optimize as opt
+
+# particle lifetimes and uncertainties (muon, pion)
+tau = (2.1969811e-6, 2.6033e-8)
+tau_unc = (0.0000022e-6, 0.0005e-8)
+
+# variables
+tBound = (1e-10, 3e-5)
+
