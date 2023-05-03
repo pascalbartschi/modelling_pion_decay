fe9077be2eda1a06199b230d143230ee1fb1a57f
unknown <levinlinus.martschke@uzh.ch>
2023-05-03 09:08:27 +0200

    Can only really judge fit if we look at t-range (0,3e-5), so added xlim -> issue: due to the Gaussian smear, most tVals created are outside of this range...

--- lukasz_da.py
+++ lukasz_da.py
@@ -106,8 +106,9 @@ def four(sigmaTvals=[1/100, 1/10, 1*tau[1]]):
         counts, edges = np.histogram(tValsFour, bins=nBins)
         wBins = wBin(max(edges), min(edges), len(edges)-1)
         cBins = edges[:-1] + wBins/2
-        plt.bar(cBins, counts, width=wBins, label="Generated Decay Times", alpha=0.5)
+        #plt.bar(cBins, counts, width=wBins, label="Generated Decay Times", alpha=0.5)
         plt.plot(tValsFour,N(tValsFour,out)/200,".",markersize=1)
+        plt.xlim(0,3e-5)
         plt.xlabel("t [s]")
         plt.ylabel("Number of entries")
         plt.title("3(a) Histogram of 10'000 simulated decay times")
